807fffb22213e2f4d8ffd425ffe3dbaa693f4aa3
user50 <oor1@ukr.net>
2015-08-09 19:58:13 +0300

    fix project

--- yml-merge/src/main/java/com/company/Main.java
+++ yml-merge/src/main/java/com/company/Main.java
@@ -5,10 +5,11 @@ import com.company.config.ConfigProvider;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
+    public static void main(String[] args) throws IOException, XMLStreamException {
         Config config = new ConfigProvider().get();
 
         new MergeService().process(config);
