877772977494aaf5a13b607b8579ed03a32d1ffc
EvgenLozov <jmen7070@gmail.com>
2015-11-13 13:12:14 +0200

    auto merge:  trigger building is improved

--- yml-merger-web/src/main/java/com/merger/scheduler/TriggerFactory.java
+++ yml-merger-web/src/main/java/com/merger/scheduler/TriggerFactory.java
@@ -4,10 +4,13 @@ import com.company.config.Config;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Logger;
 
 import static org.quartz.CalendarIntervalScheduleBuilder.calendarIntervalSchedule;
 import static org.quartz.DateBuilder.todayAt;
+import static org.quartz.DateBuilder.tomorrowAt;
 
 /**
  * Created by Yevhen on 2015-08-02.
@@ -24,11 +27,23 @@ public class TriggerFactory {
         Integer hours = Integer.valueOf(config.getTime().split(":")[0]);
         Integer minutes = Integer.valueOf(config.getTime().split(":")[1]);
 
-        return TriggerBuilder
-                .newTrigger()
-                .withIdentity(jobKeyFactory.get(config).getName())
-                .startAt(todayAt(hours, minutes, 0))
-                .withSchedule(calendarIntervalSchedule().withIntervalInDays(config.getPeriod()))
-                .build();
+        TriggerBuilder triggerBuilder = TriggerBuilder
+                                        .newTrigger()
+                                        .withIdentity(jobKeyFactory.get(config).getName())
+                                        .withSchedule(calendarIntervalSchedule()
+                                        .withIntervalInDays(config.getPeriod()));
+
+        if (isBefore(hours, minutes, new Date()))
+            return triggerBuilder.startAt(todayAt(hours, minutes, 0)).build();
+        else
+            return triggerBuilder.startAt(tomorrowAt(hours, minutes, 0)).build();
+    }
+
+    private boolean isBefore(Integer hours, Integer minutes, Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return hours > cal.get(Calendar.HOUR_OF_DAY) ||
+                (hours == cal.get(Calendar.HOUR_OF_DAY) && minutes > cal.get(Calendar.MINUTE));
+
     }
 }
