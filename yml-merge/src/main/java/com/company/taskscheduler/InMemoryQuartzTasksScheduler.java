8ad170b26445f544d057d06103d3747b7bc8fa5e
EvgenLozov <jmen7070@gmail.com>
2016-01-09 11:50:06 +0200

    new  interfaces for scheduler are introduced

---
+++ yml-merge/src/main/java/com/company/taskscheduler/InMemoryQuartzTasksScheduler.java
@@ -0,0 +1,27 @@
+package com.company.taskscheduler;
+
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+
+/**
+ * Created by yevhen on 08.01.16.
+ */
+public class InMemoryQuartzTasksScheduler implements QuartzTasksScheduler{
+    private Scheduler scheduler;
+
+    public InMemoryQuartzTasksScheduler(Scheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    public void schedule(QuartzTask task) throws SchedulerException {
+        scheduler.scheduleJob(task.jobDetail(), task.trigger());
+    }
+
+    public void delete(QuartzTask task) throws SchedulerException {
+        JobKey jobKey = task.jobKey();
+        if (scheduler.checkExists(jobKey)){
+            scheduler.deleteJob(jobKey);
+        }
+    }
+}
---
+++ yml-merge/src/main/java/com/company/taskscheduler/PersistentQuartzTasksScheduler.java
@@ -0,0 +1,30 @@
+package com.company.taskscheduler;
+
+import org.quartz.SchedulerException;
+
+/**
+ * Created by yevhen on 09.01.16.
+ */
+public class PersistentQuartzTasksScheduler implements QuartzTasksScheduler {
+
+    private QuartzTasksScheduler quartzTasksScheduler;
+    private QuartzTaskRepository quartzTaskRepository;
+
+    public PersistentQuartzTasksScheduler(QuartzTasksScheduler quartzTasksScheduler,
+                                          QuartzTaskRepository quartzTaskRepository) {
+        this.quartzTasksScheduler = quartzTasksScheduler;
+        this.quartzTaskRepository = quartzTaskRepository;
+    }
+
+    @Override
+    public void schedule(QuartzTask task) throws SchedulerException {
+        quartzTaskRepository.save(task);
+        quartzTasksScheduler.schedule(task);
+    }
+
+    @Override
+    public void delete(QuartzTask task) throws SchedulerException {
+        quartzTaskRepository.delete(task);
+        quartzTasksScheduler.delete(task);
+    }
+}
