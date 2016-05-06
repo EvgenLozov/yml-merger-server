25be8bc29d867997051072840ae1a26f91356f37
User <oleogin@gmail.com>
2016-05-06 11:42:21 +0300

    refactoring

--- yml-modifier/src/main/java/com/company/ModifyService.java
+++ yml-modifier/src/main/java/com/company/ModifyService.java
@@ -1,20 +1,18 @@
 package com.company;
 
-import com.company.ModifierConfig;
-import com.company.ModifierXmlEventHandlerProvider;
-import company.DeleteOldPrices;
 import company.StAXService;
-import company.config.Config;
-import company.handlers.xml.WriteToLimitSizeFile;
 import company.handlers.xml.XmlEventHandler;
 import company.http.*;
+import company.providers.ByteArrayXmlEventReaderProvider;
 import company.providers.FileXMLEventReaderProvider;
+import company.providers.InputStreamXmlEventReaderProvider;
 import company.providers.XMLEventReaderProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -24,10 +22,9 @@ public class ModifyService {
 
     public void process(ModifierConfig config)  {
 
+        byte[] inputXmlBytes = new ModifierXmlBytesProvider(config).get();
 
-        XMLEventReaderProvider readerProvider = config.getInputFileURL() !=null && !config.getInputFileURL().isEmpty() ?
-                    getHttpReaderProvider(config):
-                    new FileXMLEventReaderProvider(config.getInputFile(), config.getEncoding());
+        XMLEventReaderProvider readerProvider = new ByteArrayXmlEventReaderProvider(inputXmlBytes, config.getEncoding());
 
         StAXService stAXService = new StAXService( readerProvider );
 
@@ -39,28 +36,5 @@ public class ModifyService {
             e.printStackTrace();
         }
 
-    }
-
-    private XMLEventReaderProvider getHttpReaderProvider(ModifierConfig config)
-    {
-        try {
-            CloseableHttpClient httpClient;
-            try {
-                httpClient = new HttpClientProvider(config.getUser(), config.getPsw()).get();
-            } catch (Exception e){
-                e.printStackTrace();
-                throw new RuntimeException("Unable to get HttpClient");
-            }
-            HttpService httpService = new HttpService(httpClient);
-            HttpRequestProvider requestProvider = new DownloadPriceListRequest(config.getInputFileURL());
-            HttpResponseHandler<String> responseHandler = new SaveIntoFileHttpResponseHandler(config.getEncoding());
-
-            String tmpFile = httpService.execute(requestProvider, responseHandler);
-
-            return new FileXMLEventReaderProvider(tmpFile, config.getEncoding());
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Unable to do auto merge of " + config.getId());
-        }
     }
 }
