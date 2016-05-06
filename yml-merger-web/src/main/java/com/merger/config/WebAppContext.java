b23cac3088bc907c71369fb42723784f975ec6f0
EvgenLozov <jmen7070@gmail.com>
2016-05-06 17:43:57 +0300

    MongoAccessLogValve has been introduced

--- yml-merger-web/src/main/java/com/merger/config/WebAppContext.java
+++ yml-merger-web/src/main/java/com/merger/config/WebAppContext.java
@@ -1,5 +1,9 @@
 package com.merger.config;
 
+import me.chanjar.tomcat.valves.MongoAccessLogValve;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -8,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author Yevhen
  */
 @Configuration
-public class WebAppContext extends WebMvcConfigurerAdapter {
+public class WebAppContext extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer{
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -17,5 +21,16 @@ public class WebAppContext extends WebMvcConfigurerAdapter {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        if (container instanceof TomcatEmbeddedServletContainerFactory) {
+            TomcatEmbeddedServletContainerFactory factory = (TomcatEmbeddedServletContainerFactory) container;
+            MongoAccessLogValve valve = new MongoAccessLogValve();
+            valve.setUri("mongodb://46.101.166.4/tomcat");
+            valve.setPattern("all");
+            factory.addContextValves(valve);
+        }
     }
 }
