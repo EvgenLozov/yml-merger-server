511d00aca7c4ed95a0bf8a1287ab753183d3510d
EvgenLozov <jmen7070@gmail.com>
2016-05-08 13:41:37 +0300

    profiles are introduced

--- yml-modifier-web/src/main/java/com/modifier/web/config/ProdWebAppContext.java
+++ yml-modifier-web/src/main/java/com/modifier/web/config/ProdWebAppContext.java
@@ -1,18 +1,24 @@
 package com.modifier.web.config;
 
 import me.chanjar.tomcat.valves.MongoAccessLogValve;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * @author Yevhen
- */
 @Configuration
-public class WebAppContext extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer{
+@Profile("prod")
+public class ProdWebAppContext extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer{
+
+    public static final Logger logger = LoggerFactory.getLogger(ProdWebAppContext.class.getSimpleName());
+
+    public static final String MONGO_URI = "mongodb://46.101.166.4/tomcat";
+    public static final String LOGGING_PATTERN = "all";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -26,10 +32,11 @@ public class WebAppContext extends WebMvcConfigurerAdapter implements EmbeddedSe
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
         if (container instanceof TomcatEmbeddedServletContainerFactory) {
+            logger.info("Customize embedded servlet container");
             TomcatEmbeddedServletContainerFactory factory = (TomcatEmbeddedServletContainerFactory) container;
             MongoAccessLogValve valve = new MongoAccessLogValve();
-            valve.setUri("mongodb://46.101.166.4/tomcat");
-            valve.setPattern("all");
+            valve.setUri(MONGO_URI);
+            valve.setPattern(LOGGING_PATTERN);
             factory.addContextValves(valve);
         }
     }
